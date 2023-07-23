c301c04379bacd57020f888f8815ec6a7993d481
donggyuyoo <donnieyoo88@gmail.com>
2023-07-24 03:00:27 +0900

    Create Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from '../core/heropy';
+
+export default new Store({
+  photo: 'https://heropy.blog/css/images/logo.png',
+  name: 'DONNIE / Yoo,Donggyu',
+  email: 'jp.dg.yoo@gmail.com',
+  blog: 'https://heropy.blog',
+  github: 'https://github.com/donggyuyoo', 
+  repository: 'https://github.com/ParkYoungWoong/vanillajs-movie-app'
+})
